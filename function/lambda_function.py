cc8d5ca0d1b4b2a6d79b005f0f9fd6d13a1fde18
ndilsou <nasoungadoy@gmail.com>
2021-12-08 12:39:28 +0000

    improve function

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -10,10 +10,4 @@ def lambda_handler(event, context):
     """
     print('Starting functions\n---------------------------------------------')
 
-    if event["input"] == "Hello":
-
-        return "World"
-
-    else:
-
-        raise
+    return f"{event['input']} World"
